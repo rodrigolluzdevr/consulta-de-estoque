9eec1ff4d46a8c9c9bee2af098563765afb73c3e
rodrigolluzdevr <rodrigolluzdevr@gmail.com>
2024-05-09 10:59:12 -0300

    chore: config bootstrap

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <title>Seleção de Departamentos e Produtos</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <script>
@@ -41,20 +43,46 @@
     });
 </script>
 
-
-<body >
-    <p>
-        Escolha o Departamento:<br />
-        <select name="departamentos" id="departamentos">
-            <?= $options_departamentos ?>
-        </select>
-    </p>
-    <p>
-        Escolha o Produto:<br />
-        <select name="produtos" id="produtos">
-        </select>
-    </p>
-
+<body>
+    <div class="container mt-10 bg-transparent">
+        <div>
+            <!--begin::Title-->
+            <h2 class="fs-2x fw-bold p-5 d-flex justify-content-center text-dark">Consulta de Estoque</h2>
+            <!--end::Title-->
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="p-0">
+                    <!--begin::Wrapper-->
+                    <div class="text-center py-20 my-10">
+                        <!--begin::Description-->
+                        <p class="text-gray-400 fs-4 fw-semibold mb-10 text-dark opacity-75">Escolha o Departamento:</p>
+                        <select class="form-control mb-4" name="departamentos" id="departamentos">
+                            <?= $options_departamentos ?>
+                        </select>
+                        <!--end::Description-->
+                    </div>
+                    <!--end::Wrapper-->
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-0">
+                    <!--begin::Wrapper-->
+                    <div class="text-center py-20 my-10">
+                        <!--begin::Description-->
+                        <p class="text-gray-400 fs-4 fw-semibold mb-10 text-dark opacity-75">Escolha o Produto:</p>
+                        <select class="form-control mb-4" name="produtos" id="produtos">
+                        </select>
+                        <!--end::Description-->
+                    </div>
+                    <!--end::Wrapper-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center">
+        <img src="app/images/g7sistemas.webp" alt="G7 Sistemas" class="img-fluid d-block mx-auto mb-5" style="max-width: 200px;">
+    </div>
 </body>
 
 </html>
